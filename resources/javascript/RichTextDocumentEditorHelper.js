3d53c8485c970ad887e13c5e86ecc99c2fabe409
lapiukshtiss <martynas.stake@gmail.com>
2015-03-05 12:41:30 +0200

    Rich text editor component

---
+++ resources/javascript/RichTextDocumentEditorHelper.js
@@ -0,0 +1,28 @@
+tinyMCE.init({
+	selector:'textarea',
+	height : '100%',
+	width : '100%',
+	setup : function(ed) {
+		ed.onChange.add(function(ed, l) {
+			var processDefinitionId = jQuery("input[id='editorForm:oldProcessDefinitionId']").val();
+			RichTextDocumentService.updateTempolarPDF(processDefinitionId, tinyMCE.activeEditor.getContent(), {
+				callback: function(result) {
+					jQuery("a.windowLink").attr("href", result);
+				}
+			});
+		});
+	}
+});
+
+jQuery(document).ready(function() {
+	jQuery(".windowLink").fancybox({
+		type:		"iframe",
+		autoSize:	false,
+		beforeShow:	function () {
+			var iframe = jQuery(".fancybox-iframe");
+			if (iframe != null) {
+				this.width = 800;
+			}
+		}
+	});
+});
