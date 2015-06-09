457b2baa534cc430abc9e14a6f593cc9bf6f6e28
Valdas Žemaitis <valdas@idega.com>
2015-06-09 12:06:20 +0300

    Bug fix for loading image from local repository

--- src/java/com/idega/block/pdf/presentation/handler/Base64ImageTagProcessor.java
+++ src/java/com/idega/block/pdf/presentation/handler/Base64ImageTagProcessor.java
@@ -3,82 +3,82 @@
  *
  * Idega Software hf. Source Code Licence Agreement x
  *
- * This agreement, made this 10th of February 2006 by and between 
- * Idega Software hf., a business formed and operating under laws 
- * of Iceland, having its principal place of business in Reykjavik, 
- * Iceland, hereinafter after referred to as "Manufacturer" and Agura 
+ * This agreement, made this 10th of February 2006 by and between
+ * Idega Software hf., a business formed and operating under laws
+ * of Iceland, having its principal place of business in Reykjavik,
+ * Iceland, hereinafter after referred to as "Manufacturer" and Agura
  * IT hereinafter referred to as "Licensee".
- * 1.  License Grant: Upon completion of this agreement, the source 
- *     code that may be made available according to the documentation for 
- *     a particular software product (Software) from Manufacturer 
- *     (Source Code) shall be provided to Licensee, provided that 
- *     (1) funds have been received for payment of the License for Software and 
- *     (2) the appropriate License has been purchased as stated in the 
- *     documentation for Software. As used in this License Agreement, 
- *     Licensee shall also mean the individual using or installing 
- *     the source code together with any individual or entity, including 
- *     but not limited to your employer, on whose behalf you are acting 
- *     in using or installing the Source Code. By completing this agreement, 
- *     Licensee agrees to be bound by the terms and conditions of this Source 
- *     Code License Agreement. This Source Code License Agreement shall 
- *     be an extension of the Software License Agreement for the associated 
- *     product. No additional amendment or modification shall be made 
- *     to this Agreement except in writing signed by Licensee and 
+ * 1.  License Grant: Upon completion of this agreement, the source
+ *     code that may be made available according to the documentation for
+ *     a particular software product (Software) from Manufacturer
+ *     (Source Code) shall be provided to Licensee, provided that
+ *     (1) funds have been received for payment of the License for Software and
+ *     (2) the appropriate License has been purchased as stated in the
+ *     documentation for Software. As used in this License Agreement,
+ *     Licensee shall also mean the individual using or installing
+ *     the source code together with any individual or entity, including
+ *     but not limited to your employer, on whose behalf you are acting
+ *     in using or installing the Source Code. By completing this agreement,
+ *     Licensee agrees to be bound by the terms and conditions of this Source
+ *     Code License Agreement. This Source Code License Agreement shall
+ *     be an extension of the Software License Agreement for the associated
+ *     product. No additional amendment or modification shall be made
+ *     to this Agreement except in writing signed by Licensee and
  *     Manufacturer. This Agreement is effective indefinitely and once
- *     completed, cannot be terminated. Manufacturer hereby grants to 
- *     Licensee a non-transferable, worldwide license during the term of 
- *     this Agreement to use the Source Code for the associated product 
- *     purchased. In the event the Software License Agreement to the 
- *     associated product is terminated; (1) Licensee's rights to use 
- *     the Source Code are revoked and (2) Licensee shall destroy all 
- *     copies of the Source Code including any Source Code used in 
+ *     completed, cannot be terminated. Manufacturer hereby grants to
+ *     Licensee a non-transferable, worldwide license during the term of
+ *     this Agreement to use the Source Code for the associated product
+ *     purchased. In the event the Software License Agreement to the
+ *     associated product is terminated; (1) Licensee's rights to use
+ *     the Source Code are revoked and (2) Licensee shall destroy all
+ *     copies of the Source Code including any Source Code used in
  *     Licensee's applications.
  * 2.  License Limitations
- *     2.1 Licensee may not resell, rent, lease or distribute the 
- *         Source Code alone, it shall only be distributed as a 
+ *     2.1 Licensee may not resell, rent, lease or distribute the
+ *         Source Code alone, it shall only be distributed as a
  *         compiled component of an application.
- *     2.2 Licensee shall protect and keep secure all Source Code 
- *         provided by this this Source Code License Agreement. 
- *         All Source Code provided by this Agreement that is used 
+ *     2.2 Licensee shall protect and keep secure all Source Code
+ *         provided by this this Source Code License Agreement.
+ *         All Source Code provided by this Agreement that is used
  *         with an application that is distributed or accessible outside
- *         Licensee's organization (including use from the Internet), 
- *         must be protected to the extent that it cannot be easily 
+ *         Licensee's organization (including use from the Internet),
+ *         must be protected to the extent that it cannot be easily
  *         extracted or decompiled.
- *     2.3 The Licensee shall not resell, rent, lease or distribute 
- *         the products created from the Source Code in any way that 
+ *     2.3 The Licensee shall not resell, rent, lease or distribute
+ *         the products created from the Source Code in any way that
  *         would compete with Idega Software.
- *     2.4 Manufacturer's copyright notices may not be removed from 
+ *     2.4 Manufacturer's copyright notices may not be removed from
  *         the Source Code.
- *     2.5 All modifications on the source code by Licencee must 
+ *     2.5 All modifications on the source code by Licencee must
  *         be submitted to or provided to Manufacturer.
- * 3.  Copyright: Manufacturer's source code is copyrighted and contains 
- *     proprietary information. Licensee shall not distribute or 
- *     reveal the Source Code to anyone other than the software 
- *     developers of Licensee's organization. Licensee may be held 
- *     legally responsible for any infringement of intellectual property 
- *     rights that is caused or encouraged by Licensee's failure to abide 
- *     by the terms of this Agreement. Licensee may make copies of the 
- *     Source Code provided the copyright and trademark notices are 
- *     reproduced in their entirety on the copy. Manufacturer reserves 
+ * 3.  Copyright: Manufacturer's source code is copyrighted and contains
+ *     proprietary information. Licensee shall not distribute or
+ *     reveal the Source Code to anyone other than the software
+ *     developers of Licensee's organization. Licensee may be held
+ *     legally responsible for any infringement of intellectual property
+ *     rights that is caused or encouraged by Licensee's failure to abide
+ *     by the terms of this Agreement. Licensee may make copies of the
+ *     Source Code provided the copyright and trademark notices are
+ *     reproduced in their entirety on the copy. Manufacturer reserves
  *     all rights not specifically granted to Licensee.
  *
- * 4.  Warranty & Risks: Although efforts have been made to assure that the 
- *     Source Code is correct, reliable, date compliant, and technically 
- *     accurate, the Source Code is licensed to Licensee as is and without 
- *     warranties as to performance of merchantability, fitness for a 
- *     particular purpose or use, or any other warranties whether 
- *     expressed or implied. Licensee's organization and all users 
- *     of the source code assume all risks when using it. The manufacturers, 
- *     distributors and resellers of the Source Code shall not be liable 
- *     for any consequential, incidental, punitive or special damages 
- *     arising out of the use of or inability to use the source code or 
- *     the provision of or failure to provide support services, even if we 
- *     have been advised of the possibility of such damages. In any case, 
- *     the entire liability under any provision of this agreement shall be 
- *     limited to the greater of the amount actually paid by Licensee for the 
- *     Software or 5.00 USD. No returns will be provided for the associated 
- *     License that was purchased to become eligible to receive the Source 
- *     Code after Licensee receives the source code. 
+ * 4.  Warranty & Risks: Although efforts have been made to assure that the
+ *     Source Code is correct, reliable, date compliant, and technically
+ *     accurate, the Source Code is licensed to Licensee as is and without
+ *     warranties as to performance of merchantability, fitness for a
+ *     particular purpose or use, or any other warranties whether
+ *     expressed or implied. Licensee's organization and all users
+ *     of the source code assume all risks when using it. The manufacturers,
+ *     distributors and resellers of the Source Code shall not be liable
+ *     for any consequential, incidental, punitive or special damages
+ *     arising out of the use of or inability to use the source code or
+ *     the provision of or failure to provide support services, even if we
+ *     have been advised of the possibility of such damages. In any case,
+ *     the entire liability under any provision of this agreement shall be
+ *     limited to the greater of the amount actually paid by Licensee for the
+ *     Software or 5.00 USD. No returns will be provided for the associated
+ *     License that was purchased to become eligible to receive the Source
+ *     Code after Licensee receives the source code.
  */
 package com.idega.block.pdf.presentation.handler;
 
@@ -97,7 +97,11 @@ import javax.imageio.ImageIO;
 
 import com.idega.idegaweb.IWMainApplication;
 import com.idega.idegaweb.IWMainApplicationSettings;
+import com.idega.repository.RepositoryService;
+import com.idega.util.CoreConstants;
+import com.idega.util.StringHandler;
 import com.idega.util.StringUtil;
+import com.idega.util.expression.ELUtil;
 import com.itextpdf.text.Chunk;
 import com.itextpdf.text.Element;
 import com.itextpdf.text.Image;
@@ -109,7 +113,7 @@ import com.itextpdf.tool.xml.pipeline.html.HtmlPipelineContext;
 
 /**
  * <p>Parses base64 image link</p>
- * <p>You can report about problems to: 
+ * <p>You can report about problems to:
  * <a href="mailto:martynas@idega.is">Martynas Stakė</a></p>
  *
  * @version 1.0.0 2015 June 8
@@ -128,9 +132,9 @@ public class Base64ImageTagProcessor extends com.itextpdf.tool.xml.html.Image {
 
 		return null;
 	}
-	
+
 	/**
-	 * 
+	 *
 	 * @param image to convert, not <code>null</code>;
 	 * @return image converted to bytes or <code>null</code> on failure;
 	 * @author <a href="mailto:martynas@idega.is">Martynas Stakė</a>
@@ -143,7 +147,7 @@ public class Base64ImageTagProcessor extends com.itextpdf.tool.xml.html.Image {
 				ImageIO.write(image, getImageType(), baos);
 				LOGGER.info("Image converted to byte output stream.");
 			} catch (IOException e) {
-				LOGGER.log(Level.WARNING, 
+				LOGGER.log(Level.WARNING,
 						"Failed to copy image byte stream, cause of: ", e);
 			}
 
@@ -151,7 +155,7 @@ public class Base64ImageTagProcessor extends com.itextpdf.tool.xml.html.Image {
 				baos.flush();
 				LOGGER.info("Byte stream has been flushed!");
 			} catch (IOException e) {
-				LOGGER.log(Level.WARNING, 
+				LOGGER.log(Level.WARNING,
 						"Failed to flush stream, cause of: ", e);
 			}
 
@@ -168,25 +172,41 @@ public class Base64ImageTagProcessor extends com.itextpdf.tool.xml.html.Image {
 
 		return imageInBytes;
 	}
-	
+
 	/**
-	 * 
+	 *
 	 * @param url for example www.google.com, not <code>null</code>;
 	 * @return image found by link or <code>null</code> on failure;
 	 * @author <a href="mailto:martynas@idega.is">Martynas Stakė</a>
 	 */
 	public BufferedImage getImage(String url) {
-		if (!StringUtil.isEmpty(url)) {
-			try {
+		InputStream stream = null;
+		if (StringUtil.isEmpty(url)) {
+			LOGGER.warning("URL is not provided");
+			return null;
+		}
+
+		try {
+			if (url.indexOf("../") != -1) {
+				url = StringHandler.replace(url, "../", CoreConstants.EMPTY);
+				if (url.startsWith("content/files/")) {
+					url = CoreConstants.SLASH + url;
+				}
+			}
+
+			if (url.startsWith(CoreConstants.WEBDAV_SERVLET_URI)) {
+				RepositoryService repository = ELUtil.getInstance().getBean(RepositoryService.BEAN_NAME);
+				stream = repository.getInputStreamAsRoot(url);
+			} else {
 				URL outsideUrl = new URL(url);
 				outsideUrl.openConnection();
-				InputStream inputStream = outsideUrl.openStream();
-				LOGGER.info("Image was prepared for reading!");
-				return ImageIO.read(inputStream);
-			} catch (Exception e) {
-				LOGGER.log(Level.WARNING, 
-						"Failed to get image by URL: '" + url + "' cause of: ", e);
+				stream = outsideUrl.openStream();
 			}
+
+			LOGGER.info("Image was prepared for reading!");
+			return ImageIO.read(stream);
+		} catch (Exception e) {
+			LOGGER.log(Level.WARNING, "Failed to get image by URL: '" + url + "' cause of: ", e);
 		}
 
 		return null;
@@ -213,10 +233,10 @@ public class Base64ImageTagProcessor extends com.itextpdf.tool.xml.html.Image {
 
 		return null;
 	}
-	
+
 	@Override
 	public List<Element> end(
-			WorkerContext ctx, 
+			WorkerContext ctx,
 			Tag tag,
 			List<Element> currentContent) {
 	    List<Element> elements = new ArrayList<Element>(1);
@@ -235,7 +255,7 @@ public class Base64ImageTagProcessor extends com.itextpdf.tool.xml.html.Image {
 
 	            if (img != null) {
 	            	LOGGER.info("Image was successfully parsed, preparing to write to PDF");
-	            	
+
 	                try {
 	                    final HtmlPipelineContext htmlPipelineContext = getHtmlPipelineContext(ctx);
 	                    elements.add(getCssAppliers().apply(new Chunk((com.itextpdf.text.Image) getCssAppliers().apply(img, tag, htmlPipelineContext), 0, 0, true), tag,
